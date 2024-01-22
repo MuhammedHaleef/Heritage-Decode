544aae2ab99749a45ed2d4530d2ce63edb4ca743
ChavinduDulaj <chavindudulaj18249@gmail.com>
2024-01-22 19:23:42 +0530

    Change the home screen widgets and  create a second screen

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -1,33 +1,87 @@
 // main.dart
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
-
     );
   }
 }
 
-// Creating homepage for heritage decode mobile application
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Heritage Decode', style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        backgroundColor: const Color.fromARGB(255, 62, 119, 167),
       ),
-      body: Center(
-        child: Text('Welcome to Heritage Decode!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/Logo.png',
+              height: 300.0,
+              width: 300.0,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          OutlinedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage()),
+              );
+            },
+            child: Text(
+              'Get Started',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page', style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: const Color.fromARGB(255, 62, 119, 167),
+      ),
+      body: Center(
+        child: Text(
+          'This is the Second Page',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
