4d717945e48f6cafce27767a29c9ab9c0e3f5656
ChavinduDulaj <chavindudulaj18249@gmail.com>
2024-01-25 22:30:11 +0530

    Create a button to get started and also added Logo to home screen

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -21,7 +21,48 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar : AppBar(
         title: Text('Heritage Decode', style: TextStyle(color: Colors.white),) ,
-        backgroundColor: const Color.fromARGB(255, 62, 119, 167),)
+        backgroundColor: const Color.fromARGB(255, 62, 119, 167),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/Logo.png',
+              height: 300.0,
+              width: 300.0
+            ),
+          ),
+          SizedBox(height: 20.0),
+          OutlinedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage()),
+              );
+            },
+             child: Text(
+              'Get Started',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          )
+        ]
+        ),
+        
     );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
