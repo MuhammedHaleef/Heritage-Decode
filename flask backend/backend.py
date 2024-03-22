ac6377f3ed4bade4485af70a682d6dd3079ed937
ChavinduDulaj <chavindudulaj18249@gmail.com>
2024-03-21 22:50:02 -0700

    added functions for translating text, predictions and output as well

--- flask backend/backend.py
+++ flask backend/backend.py
@@ -9,9 +9,16 @@ app = Flask(__name__)
 # model = tf.keras.models.load_model('wdwdwdwdwdwdwdwdwdwdwdwwdwdwd')
 
 
+
 # Do the preprocess part when image came to backend
 def preprocess():
     print("Do the preprocess here")
+
+
+
+# Translating from the database (Lakindu's part)
+def translate():
+    print("Lakindu's part here")
 
 
 # testing the server
@@ -27,6 +34,36 @@ def upload_and_translate():
         if 'image' not in request.files:
             return jsonify({'error': 'No image file found'}), 400
         image_file = request.files['image']
+        
+        
+    
+        temp_image_path = 'temp_image.jpg'
+        image_file.save(temp_image_path)
+
+        processed_image = preprocess(temp_image_path)
+        
+    
+        prediction = model.predict(processed_image)
+        # call the lakindu's translating function and checking with the database
+
+        translated_text = " "
+        segmented_image = []
+
+        # After that show the original image, segmanted image and the translated text here
+
+        os.remove(temp_image_path)
+
+        return jsonify({
+                   "Translated Text":translated_text,
+                    "Segmented Image":segmented_image
+                       })
+
+        
 
 
 
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)  
